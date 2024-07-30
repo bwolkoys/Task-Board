516487ad6620d76178f29c77baecd667ea9f419d
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-07-30 14:02:27 -0700

    added modal to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ function generateTaskId() {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
     let deadlineDate = "Deadline Date"
-    let title = "Title"
+    let title = document.getElementById("taskName").value;
     let description = "Description"
     let div = document.createElement("div");
     let h3 = document.createElement("h3");
@@ -38,7 +38,21 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-    createTaskCard()
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("add_task");
+    //var span = document.getElementsByClassName("close")[0];
+    //btn.onclick = function() {
+        modal.style.display = "block";
+     // }
+     /*span.onclick = function() {
+        modal.style.display = "none";
+      }
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }*/
+   // createTaskCard() 
 }
 
 // Todo: create a function to handle deleting a task
@@ -54,5 +68,5 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
     document.getElementById("add_task").addEventListener("click", handleAddTask);
-
+    document.getElementById("createTask").addEventListener("click", createTaskCard);
 });
