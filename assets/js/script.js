8b4eb345bcfbc819fb0b035fe62afba7bf5785fe
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-07-31 14:50:03 -0700

    fixed the modal and pulled info from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
+//let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
@@ -17,33 +17,32 @@ function createTaskCard(task) {
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
     let button = document.createElement("button");
-    h3.textContent = title
-    p1.textContent = description 
-    p2.textContent = deadlineDate
+    h3.textContent = task.title
+    p1.textContent = task.description 
+    p2.textContent = task.deadlineDate
     button.textContent = "Delete"
+    div.classList.add('task-card');
     div.appendChild(h3);
     div.appendChild(p1);
     div.appendChild(p2);
     div.appendChild(button);
-    document.getElementById("todo-cards").appendChild(div);
+    return div 
+    //document.getElementById("todo-cards").appendChild(div);
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    const taskList = document.getElementById('taskList');
+    const taskList = document.getElementById('todo-cards');
     //using this to clear existing task list
     taskList.innerHTML = '';
     tasks.forEach(task =>{
-        const taskCard = document.createElement('div');
-        taskCard.classList.add('task-card');
-        taskCard.textContent = task.title;
+        const taskCard = createTaskCard (task);
+        //taskCard.textContent = task.title;
         //to make it draggable
         taskCard.draggable = true;
         taskList.appendChild(taskCard);
     })
-    //calling the function
-    renderTaskList();
 }
 
 // Todo: create a function to handle adding a new task
@@ -56,15 +55,29 @@ function handleAddTask(event){
     task.title = title
     task.deadlineDate = deadlineDate
     task.description = description 
-    taskList.push(task);
-    localStorage.setItem('tasks', JSON.stringify(taskList));
-   // createTaskCard() 
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    console.log(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    renderTaskList();
 }
 
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
+    
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("add_task");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function() {
     modal.style.display = "block";
   }
   span.onclick = function() {
@@ -75,60 +88,8 @@ btn.onclick = function() {
       modal.style.display = "none";
     }
   }
-
-
-/*function popUpModal(event) {
-    const modal = document.getElementById("myModal");
-    const createTask = document.getElementById("createTask");
-    const closeBtn = document.querySelector('.close');
-    const taskForm = document.getElementById('taskForm');
-    createTask.addEventListener('click', () => {
-        modal.style.display = 'block';
-    });
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
-    window.addEventListener('click', (event) => {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    });
-    taskForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        const taskName = document.getElementById('taskName').value;
-        tasks.push({tasks: taskName});
-        taskForm.reset();
-        modal.style.display = 'none';
-        renderTaskList();
-    });
-}*/
-    //var span = document.getElementsByClassName("close")[0];
-    //btn.onclick = function() {
-        //modal.style.display = "block";
-     // }
-     /*span.onclick = function() {
-        modal.style.display = "none";
-      }
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
-}*/
-
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
-
-// Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
-
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-    document.getElementById("add_task").addEventListener("click", popUpModal);
+    //document.getElementById("add_task").addEventListener("click", popUpModal);
     document.getElementById("createTask").addEventListener("click", handleAddTask);
+    renderTaskList ()
 }); 
 
